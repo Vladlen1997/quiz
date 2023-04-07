014eb71b9334766c5c3d790b3eb373cc1dac2ba5
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-07 19:16:43 +0300

    created app.scss and imported into app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './index.scss';
+import './App.scss';
 
 const questions = [{
     title: 'React - это ... ?', variants: ['библиотека', 'фреймворк', 'приложение'], correct: 0,
@@ -15,32 +16,32 @@ const questions = [{
 
 function Game() {
     return (<>
-            <div className="progress">
-                <div style={{width: '50%'}} className="progress__inner"></div>
-            </div>
-            <h1>Что такое useState?</h1>
-            <ul>
-                <li>Это функция для хранения данных компонента</li>
-                <li>Это глобальный стейт</li>
-                <li>Это когда на ты никому не нужен</li>
-            </ul>
-        </>);
+        <div className="progress">
+            <div style={{width: '50%'}} className="progress__inner"></div>
+        </div>
+        <h1>Что такое useState?</h1>
+        <ul>
+            <li>Это функция для хранения данных компонента</li>
+            <li>Это глобальный стейт</li>
+            <li>Это когда на ты никому не нужен</li>
+        </ul>
+    </>);
 }
 
 
 function Result() {
     return (<div className="result">
-            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
-            <h2>Вы отгадали 3 ответа из 10</h2>
-            <button>Попробовать снова</button>
-        </div>);
+        <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
+        <h2>Вы отгадали 3 ответа из 10</h2>
+        <button>Попробовать снова</button>
+    </div>);
 }
 
 function App() {
     return (<div className="App">
-            <Game/>
-            {/* <Result /> */}
-        </div>);
+        <Game/>
+        {/* <Result /> */}
+    </div>);
 }
 
 export default App;
