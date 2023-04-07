cc4553292472be019b89c856b58b699b8b777838
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-07 19:09:47 +0300

    added result

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import './index.scss';
 
-const questions = [
-    {
-        title: 'React - это ... ?',
-        variants: ['библиотека', 'фреймворк', 'приложение'],
-        correct: 0,
-    },
-    {
-        title: 'Компонент - это ... ',
-        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-        correct: 1,
-    },
-    {
-        title: 'Что такое JSX?',
-        variants: [
-            'Это простой HTML',
-            'Это функция',
-            'Это тот же HTML, но с возможностью выполнять JS-код',
-        ],
-        correct: 2,
-    },
-];
+const questions = [{
+    title: 'React - это ... ?', variants: ['библиотека', 'фреймворк', 'приложение'], correct: 0,
+}, {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+}, {
+    title: 'Что такое JSX?',
+    variants: ['Это простой HTML', 'Это функция', 'Это тот же HTML, но с возможностью выполнять JS-код',],
+    correct: 2,
+},];
+
 
 function Game() {
-    return (
-        <>
+    return (<>
             <div className="progress">
                 <div style={{width: '50%'}} className="progress__inner"></div>
             </div>
@@ -34,17 +24,23 @@ function Game() {
                 <li>Это глобальный стейт</li>
                 <li>Это когда на ты никому не нужен</li>
             </ul>
-        </>
-    );
+        </>);
 }
 
 
-function App() {
-    return (
-        <div className="App">
+function Result() {
+    return (<div className="result">
+            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
+            <h2>Вы отгадали 3 ответа из 10</h2>
+            <button>Попробовать снова</button>
+        </div>);
+}
 
-        </div>
-    );
+function App() {
+    return (<div className="App">
+            <Game/>
+            {/* <Result /> */}
+        </div>);
 }
 
 export default App;
