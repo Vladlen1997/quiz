cb19390f63e5e56e419a06ecf8b145a9a9837d7f
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-07 20:11:13 +0300

    in width specified percentage, which will allow the scale to work

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function Game({step, question, onClickVariant}) {
     console.log(percentage)
     return (<>
         <div className="progress">
-            <div style={{width: '50%'}} className="progress__inner"></div>
+            <div style={{width: `${percentage}%`}} className="progress__inner"></div>
         </div>
         <h1>{question.title}</h1>
         <ul>
