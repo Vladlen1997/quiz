97f7ee8f1634976b1441561e47f8d736526c12d0
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-07 20:24:55 +0300

    checked the correct option

--- src/App.js
+++ src/App.js
@@ -34,27 +34,32 @@ function Game({step, question, onClickVariant}) {
 }
 
 
-function Result() {
+function Result({correct}) {
     return (<div className="result">
         <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>
-        <h2>Вы отгадали 3 ответа из 10</h2>
+        <h2>Вы отгадали {correct} ответа из {questions.length}</h2>
         <button>Попробовать снова</button>
     </div>);
 }
 
 function App() {
     const [step, setStep] = React.useState(0);
+    const [correct, setCorrect] = React.useState(0);
     const question = questions[step];
 
     const onClickVariant = (index) => {
         console.log(step, index);
         setStep(step + 1);
+
+        if (index === question.correct) {
+            setCorrect(correct + 1);
+        }
     }
     return (
         <div className="App">
             {step !== questions.length ? (
-                <Game step={step} question={question} onClickVariant={onClickVariant}/>) :
-                (<Result/>)
+                    <Game step={step} question={question} onClickVariant={onClickVariant}/>) :
+                (<Result correct={correct}/>)
             }
 
         </div>);
