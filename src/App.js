8da2e2a4c4dc883def84e68493995b4e1eeb5eae
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-04-07 19:03:41 +0300

    added questions data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.scss';
+import './index.scss';
+
+const questions = [
+    {
+        title: 'React - это ... ?',
+        variants: ['библиотека', 'фреймворк', 'приложение'],
+        correct: 0,
+    },
+    {
+        title: 'Компонент - это ... ',
+        variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+        correct: 1,
+    },
+    {
+        title: 'Что такое JSX?',
+        variants: [
+            'Это простой HTML',
+            'Это функция',
+            'Это тот же HTML, но с возможностью выполнять JS-код',
+        ],
+        correct: 2,
+    },
+];
+
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
